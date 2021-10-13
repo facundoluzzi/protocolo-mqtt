81ff8ced94263a996cb2c78ebb6acce0e2b3f4ab
facundoluzzi <facundoluzz@gmail.comgit config --global user.email facundoluzz@gmail.com>
2021-10-13 20:43:18 -0300

    Creamos el servidor

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,30 @@
+use std::io::BufRead;
+use std::io::BufReader;
+use std::net::TcpStream;
+use std::net::{TcpListener};
+
+
 fn main() {
-    println!("Hello, world!");
+    let address = "0.0.0.0:1883".to_owned();
+    let listener = TcpListener::bind(address);
+    if let Ok(listener_ok) = listener {
+        println!("{:?}", listener_ok);
+        let connection = listener_ok.accept();
+        if let Ok(connection_ok) = connection {
+            let client_stream : TcpStream = connection_ok.0;
+            let reader = BufReader::new(client_stream);
+            let mut lines = reader.lines();
+            while let Some(line) = lines.next() {
+                println!("Recibido: {:?}", line);
+            }
+        } else if let Err(connection_err) = connection {
+            println!("{:?}", connection_err);
+            return;
+        }
+    } else if let Err(err) = listener {
+        println!("{:?}", err);
+        return;
+    }
 }
 
 #[cfg(test)]
@@ -9,3 +34,5 @@ mod tests {
         assert_eq!(1, 1)
     }
 }
+
+
