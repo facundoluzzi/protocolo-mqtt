6e3d0e98b20f1909a2bc928cdbb47bf3b545d764
facundoluzzi <facundoluzz@gmail.comgit config --global user.email facundoluzz@gmail.com>
2021-10-13 21:48:47 -0300

    Ahora si, conectamos cliente

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,17 +9,15 @@ fn main() {
     let reader = BufReader::new(stdin());
     let socket = TcpStream::connect(address);
     if let Ok(mut socket_ok) = socket {
-        for line in reader.lines() {
-            if let Ok(line_ok) = line {
-                println!("Enviando: {:?}", line_ok);
-                if let Err(respuesta_uno) = socket_ok.write(line_ok.as_bytes()) {
-                    println!("{}", respuesta_uno);
-                    return;
-                }
-                if let Err(respuesta_dos) = socket_ok.write("\n".to_owned().as_bytes()) {
-                    println!("{}", respuesta_dos);
-                    return;
-                }
+        for line in reader.lines().flatten() {
+            println!("Enviando: {:?}", line);
+            if let Err(respuesta_uno) = socket_ok.write(line.as_bytes()) {
+                println!("{}", respuesta_uno);
+                return;
+            }
+            if let Err(respuesta_dos) = socket_ok.write("\n".to_owned().as_bytes()) {
+                println!("{}", respuesta_dos);
+                return;
             }
         }
     }
