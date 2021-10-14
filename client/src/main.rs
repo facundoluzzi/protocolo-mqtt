123b3de1b90aa170c2360e2e5f79f9388cbe5a49
facundoluzzi <facundoluzz@gmail.comgit config --global user.email facundoluzz@gmail.com>
2021-10-13 21:45:49 -0300

    Conectamos cliente

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,25 @@
-use std::io::BufRead;
-use std::net::TcpStream;
 use std::io::stdin;
+use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Write;
+use std::net::TcpStream;
 
 fn main() {
     let address = "0.0.0.0:1883".to_owned();
     let reader = BufReader::new(stdin());
-    let mut socket = TcpStream::connect(address);
+    let socket = TcpStream::connect(address);
     if let Ok(mut socket_ok) = socket {
         for line in reader.lines() {
             if let Ok(line_ok) = line {
                 println!("Enviando: {:?}", line_ok);
-                socket_ok.write(line_ok.as_bytes());
-                socket_ok.write("\n".as_bytes());
+                if let Err(respuesta_uno) = socket_ok.write(line_ok.as_bytes()) {
+                    println!("{}", respuesta_uno);
+                    return;
+                }
+                if let Err(respuesta_dos) = socket_ok.write("\n".to_owned().as_bytes()) {
+                    println!("{}", respuesta_dos);
+                    return;
+                }
             }
         }
     }
