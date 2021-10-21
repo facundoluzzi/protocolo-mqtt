0e6f4f31ee806bddc55e99fedd4ef01ce3c6ce10
argelignacio <iargel@fi.uba.com>
2021-10-20 21:02:28 -0300

    comienzo paquetes en client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,4 @@
+use core::unicode::conversions::to_lower;
 use std::io::stdin;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -8,19 +9,41 @@ fn main() {
     let address = "0.0.0.0:1883".to_owned();
     let reader = BufReader::new(stdin());
     let socket = TcpStream::connect(address);
+
     if let Ok(mut socket_ok) = socket {
         for line in reader.lines().flatten() {
             println!("Enviando: {:?}", line);
-            if let Err(respuesta_uno) = socket_ok.write(line.as_bytes()) {
-                println!("{}", respuesta_uno);
-                return;
-            }
-            if let Err(respuesta_dos) = socket_ok.write("\n".to_owned().as_bytes()) {
-                println!("{}", respuesta_dos);
-                return;
-            }
+            let mut toSend :u8 = match line.as_str() {
+                "CONNECT" => 0b00010000,
+                "CONNAC" => 0b00100000,
+                _ => 0b00000000
+            };
+            match toSend {
+                0b00000000 => {println!("NO PACKET");
+                    panic!()
+                }
+                _=> {
+                    if let Err(respuesta_uno) = socket_ok.write(toSend) {
+                        println!("{}", respuesta_uno);
+                        return;
+                    }
+                }
+            } 
         }
     }
+    // if let Ok(mut socket_ok) = socket {
+    //     for line in reader.lines().flatten() {
+    //         println!("Enviando: {:?}", line);
+    //         if let Err(respuesta_uno) = socket_ok.write(line.as_bytes()) {
+    //             println!("{}", respuesta_uno);
+    //             return;
+    //         }
+    //         if let Err(respuesta_dos) = socket_ok.write("\n".to_owned().as_bytes()) {
+    //             println!("{}", respuesta_dos);
+    //             return;
+    //         }
+    //     }
+    // }
 }
 
 #[cfg(test)]
---
+++ client/src/paquetes.rs
@@ -0,0 +1,88 @@
+#[derive(Debug)]
+pub enum PAQUETES {
+    CONNECT,
+    CONNAC,
+    DEFAULT
+}
+
+pub struct CONNECT {}
+
+pub struct CONNAC {}
+
+impl CONNECT {
+    pub fn get_type() -> String {
+        "connect".to_owned()   
+    }
+}
+
+impl CONNAC {
+    pub fn get_type() -> String {
+        "connac".to_owned()   
+    }
+}
+
+
+//pub struct PUBLISH {}
+
+// pub struct PUBACK {}
+
+// pub struct PUBREL {}
+
+// pub struct PUBCOMP {}
+
+// pub struct SUBSCRIBE {}
+
+// pub struct SUBACK {}
+
+// pub struct UNSUSCRIBE {}
+
+// pub struct UNSUBACK {}
+
+// pub struct PINGREQ {}
+
+// pub struct PINGRESP {}
+
+// pub struct DISCONNECT {}
+
+pub struct PacketBuilder {}
+
+impl PacketBuilder  {
+    pub fn new(linea: String) -> PAQUETES {
+        println!("linea: {}", linea);
+        // paso el string a un array de bytes
+        let bytes = linea.as_bytes();
+        println!("bytes: {:?}", bytes);
+
+        // obtengo el primer byte que tiene la configuracion
+        let first_byte = *bytes.get(0).unwrap();
+        println!("first_byte: {}", first_byte);
+
+        // me quedo con los bits del paquete
+        let mask = 0b11110000 & first_byte;
+        println!("mask: {}", mask);
+        match mask {
+            0b00010000 => {
+                PAQUETES::CONNECT{}
+            }
+            0b00100000 => {
+                PAQUETES::CONNAC{}
+            }
+            _ => {
+                PAQUETES::DEFAULT
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn crear_paquete_connect_correctamente() {
+        let bytes_packet: u8 = 0b00010000;
+        let paquete_creado = PacketBuilder::new(bytes_packet.to_string());
+        println!("{:?}", paquete_creado);
+        // assert_eq!(paquete_creado.get_type(), CONNECT{});
+    }
+}
