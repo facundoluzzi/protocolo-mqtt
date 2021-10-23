768b5f83f300225db9541d889c35893bcf4f9112
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2021-10-23 16:50:28 -0300

    Merge branch 'server/paquete_connect_remaining_length' of github.com:taller-1-fiuba-rust/21C2-ALTEGO into server/paquete_connect_remaining_length

--- server/src/paquetes.rs
+++ server/src/paquetes.rs
@@ -1,5 +1,5 @@
 pub trait Paquetes {
-    fn save_remaining_length(&mut self, bytes: &[u8]) -> Result<String, String>;
+    fn save_remaining_length(&mut self, bytes: &[u8]) -> Result<usize, String>;
     fn get_remaining_length(&self) -> usize;
     fn init(bytes: &[u8]) -> Box<dyn Paquetes>
     where
