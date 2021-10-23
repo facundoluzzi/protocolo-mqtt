f8ffaac176f8fb5060c7f724b3fcc98004ea4836
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2021-10-23 13:56:08 -0300

    agrego la lectura del remaining length al paquete connect

--- server/src/default.rs
+++ server/src/default.rs
@@ -4,7 +4,15 @@ pub struct Default {}
 
 // Implement the `Animal` trait for `Cow`.
 impl Paquetes for Default {
-    fn init() -> Box<dyn Paquetes> {
+    fn save_remaining_length(&mut self, bytes: &[u8]) -> Result<String, String> {
+        Ok("".to_string())
+    }
+
+    fn get_remaining_length(&self) -> usize {
+        0
+    }
+
+    fn init(bytes: &[u8]) -> Box<dyn Paquetes> {
         Box::new(Default {})
     }
     fn get_type(&self) -> String {
