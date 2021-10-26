38ec8f761951ffc03e17212389e041ca59fce4ad
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2021-10-25 21:02:12 -0300

    corregimos default payload

--- server/src/payload/default_payload.rs
+++ server/src/payload/default_payload.rs
@@ -1,12 +1,10 @@
 use crate::flags::flags::Flags;
 use crate::payload::payload::Payload;
 
-use crate::utf8_parser::UTF8;
-
 pub struct DefaultPayload {}
 
 impl Payload for DefaultPayload {
-    fn new(connect_flags: &Box<dyn Flags>, remaining_bytes: &[u8]) -> Box<dyn Payload> {
+    fn new(_connect_flags: &Box<dyn Flags>, _remaining_bytes: &[u8]) -> Box<dyn Payload> {
         Box::new(DefaultPayload {})
     }
 }
