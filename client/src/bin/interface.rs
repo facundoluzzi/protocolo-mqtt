25ccea09ceccfacd72628d32abff122dddce9062
IAvecilla <iavecilla@fi.uba.ar>
2021-11-04 21:55:16 -0300

    Posibilidad de la interface de conectar al cliente con el servidor

--- client/src/bin/interface.rs
+++ client/src/bin/interface.rs
@@ -1,8 +1,10 @@
 extern crate glib;
 extern crate gtk;
 
+use client::client_for_interface::Client;
 use glib::clone;
 use gtk::prelude::*;
+use std::thread;
 
 fn build_ui(app: &gtk::Application) {
     let glade_src = include_str!("test.glade");
@@ -11,12 +13,13 @@ fn build_ui(app: &gtk::Application) {
     let window: gtk::Window = builder.object("main_window").unwrap();
     window.set_application(Some(app));
     let input_port: gtk::Entry = builder.object("port_input").unwrap();
-    let button_port: gtk::Button = builder.object("port_button").unwrap();
+    let ip_input: gtk::Entry = builder.object("ip_input").unwrap();
+    let connect_button: gtk::Button = builder.object("connect_button").unwrap();
 
-    button_port.connect_clicked(clone!(@weak input_port => move |_| {
-        let port = input_port.text();
-        println!("{}", port);
-
+    connect_button.connect_clicked(clone!(@weak input_port, @weak ip_input=> move |_| {
+        let port = input_port.text().to_string();
+        let ip = ip_input.text().to_string();
+        thread::spawn(move || Client::connect_to_server(ip, port));
     }));
 
     window.show_all();
