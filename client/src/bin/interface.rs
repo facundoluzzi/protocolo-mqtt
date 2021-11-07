d028a456cf2a43fc06f45ec75f4715ccf5643147
IAvecilla <iavecilla@fi.uba.ar>
2021-11-06 21:48:05 -0300

    Cambios en interface y posiblidad de mandar connect y mantener conexion abierta con servidor

--- client/src/bin/interface.rs
+++ client/src/bin/interface.rs
@@ -15,12 +15,33 @@ fn build_ui(app: &gtk::Application) {
     let input_port: gtk::Entry = builder.object("port_input").unwrap();
     let ip_input: gtk::Entry = builder.object("ip_input").unwrap();
     let connect_button: gtk::Button = builder.object("connect_button").unwrap();
+    //let conn_window: gtk::Box = builder.object("conn_window").unwrap();
+    let result_label: gtk::Label = builder.object("result_label").unwrap();
+    let (tx, rc) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
 
-    connect_button.connect_clicked(clone!(@weak input_port, @weak ip_input=> move |_| {
+    connect_button.connect_clicked(clone!(@weak input_port, @weak ip_input => move |_| {
         let port = input_port.text().to_string();
         let ip = ip_input.text().to_string();
-        thread::spawn(move || Client::connect_to_server(ip, port));
+        let tx = tx.clone();
+        thread::spawn(move || {
+            let client = Client::new();
+            let result = client.connect_to_server(ip, port);
+            tx.send(result).unwrap();
+        });
     }));
+
+    rc.attach(None, move |text| {
+        match text {
+            Ok(label) => {
+                result_label.set_text(&label);
+            }
+            Err(label) => {
+                result_label.set_text(&label);
+            }
+        }
+
+        glib::Continue(true)
+    });
 
     window.show_all();
 }
--- client/src/client_for_interface.rs
+++ client/src/client_for_interface.rs
@@ -1,41 +1,64 @@
-use std::io::stdin;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::io::{Read, Write};
-use std::net::TcpStream;
-use std::str::from_utf8;
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+    thread,
+};
 
-pub struct Client {}
+pub struct Client {
+    stream: Option<TcpStream>,
+}
+
+impl Default for Client {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl Client {
-    pub fn connect_to_server(host: String, port: String) {
-        let reader = BufReader::new(stdin());
+    pub fn new() -> Client {
+        Client { stream: None }
+    }
+
+    fn send_connect(self) {
+        match self.stream {
+            Some(mut stream) => {
+                let bytes = [
+                    0x10, 0x0E, 0x00, 0x04, 0x4D, 0x15, 0x45, 0x45, 0x04, 0x00, 0x00, 0x0B, 0x00,
+                    0x02, 0x00, 0x00,
+                ];
+                stream.write_all(&bytes).unwrap();
+                {}
+            }
+            None => panic!("No pude enviar"),
+        }
+    }
+
+    pub fn connect_to_server(mut self, host: String, port: String) -> Result<String, String> {
         let addres = format!("{}:{}", host, port);
         match TcpStream::connect(addres) {
             Ok(mut stream) => {
-                println!("Successfully connected to server in port 3333");
-                for line in reader.lines().flatten() {
-                    if let Err(msg_error) = stream.write(line.as_bytes()) {
-                        println!("{}", msg_error);
-                        return;
-                    }
-                    println!("Sent {}, awaiting reply...", line);
-
+                let stream_clone = stream.try_clone().expect("Could not clone the stream");
+                self.stream = Some(stream_clone);
+                self.send_connect();
+                thread::spawn(move || {
                     let mut data = vec![0_u8; 100]; // using 6 byte buffer
-                    match stream.read(&mut data) {
+                    while match stream.read(&mut data) {
                         Ok(size) => {
-                            println!("received {:?}", from_utf8(&data[0..size]));
+                            println!("received {:?}", &data[0..size]);
+                            true
                         }
                         Err(e) => {
                             println!("Failed to receive data: {}", e);
+                            false
                         }
-                    }
-                }
+                    } {}
+                });
+                Ok("La conexion ha sido exitosa".to_string())
             }
             Err(e) => {
                 println!("Failed to connect: {}", e);
+                Err("La conexion no se ha podido establecer".to_string())
             }
         }
-        println!("Terminated.");
     }
 }
