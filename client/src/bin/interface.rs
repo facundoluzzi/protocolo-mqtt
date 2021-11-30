124635af5892eaf180f5de4c87d9d7a2ca73bfc9
IAvecilla <iavecilla@fi.uba.ar>
2021-11-30 01:11:13 -0300

    Cambios en la comunicacion de interfaz y cliente a traves de channels

--- client/src/bin/interface.rs
+++ client/src/bin/interface.rs
@@ -1,12 +1,24 @@
 extern crate gtk;
-
 use client::client_for_interface::Client;
-use glib::clone;
+use client::client_for_interface::ClientAction;
+use client::client_for_interface::SenderClient;
 use gtk::glib;
 use gtk::prelude::*;
-use std::thread;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
 
-fn build_objects_for_connect (builder: &gtk::Builder) -> (gtk::Entry, gtk::Entry, gtk::Entry, gtk::Entry,gtk::Entry ,gtk::Label, gtk::Button) {
+fn build_objects_for_connect(
+    builder: &gtk::Builder,
+) -> (
+    gtk::Entry,
+    gtk::Entry,
+    gtk::Entry,
+    gtk::Entry,
+    gtk::Entry,
+    gtk::Label,
+    gtk::Button,
+) {
     let input_port: gtk::Entry = builder.object("port_input").unwrap();
     let ip_input: gtk::Entry = builder.object("ip_input").unwrap();
     let connect_button: gtk::Button = builder.object("connect_button").unwrap();
@@ -15,57 +27,80 @@ fn build_objects_for_connect (builder: &gtk::Builder) -> (gtk::Entry, gtk::Entry
     let id_input: gtk::Entry = builder.object("id_input").unwrap();
     let password_input: gtk::Entry = builder.object("password_input").unwrap();
 
-    (input_port, ip_input, user_input, password_input, id_input, result_label, connect_button)
+    (
+        input_port,
+        ip_input,
+        user_input,
+        password_input,
+        id_input,
+        result_label,
+        connect_button,
+    )
 }
 
-fn build_objects_for_publish (builder: &gtk::Builder) -> (gtk::Entry, gtk::Entry, gtk::Button) {
+fn build_objects_for_publish(builder: &gtk::Builder) -> (gtk::Entry, gtk::Entry, gtk::Button) {
     let message_input: gtk::Entry = builder.object("port_input").unwrap();
     let topic_input: gtk::Entry = builder.object("ip_input").unwrap();
-    let publish_message: gtk::Button = builder.object("connect_button").unwrap();
+    let publish_message: gtk::Button = builder.object("publish_button").unwrap();
     (message_input, topic_input, publish_message)
 }
 
-fn build_ui_for_client(app: &gtk::Application, client: Client) {
+fn build_ui_for_client(app: &gtk::Application, client_sender: Sender<SenderClient>) {
+    let sender_connect = client_sender.clone();
+    let sender_publish = client_sender.clone();
     let glade_src = include_str!("test.glade");
     let builder = gtk::Builder::from_string(glade_src);
     let window: gtk::Window = builder.object("main_window").unwrap();
     window.set_application(Some(app));
-    let (input_port, ip_input, user_input, password_input, id_input, result_label, connect_button) = build_objects_for_connect(&builder);
-    let (message_input, topic_input, publish_message) = build_objects_for_publish(&builder);
+    let (input_port, ip_input, user_input, password_input, id_input, result_label, connect_button) =
+        build_objects_for_connect(&builder);
+    let (message_input, topic_input, publish_button) = build_objects_for_publish(&builder);
     let (tx, rc) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
 
-    connect_button.connect_clicked(clone!(@weak input_port, @weak ip_input, @weak password_input, @weak user_input => move |_| {
+    connect_button.connect_clicked(move |_| {
         let port = input_port.text().to_string();
         let ip = ip_input.text().to_string();
         let tx = tx.clone();
         let user = user_input.text().to_string();
         let password = password_input.text().to_string();
         let id_client = id_input.text().to_string();
-        thread::spawn(move || {
-            let client = Client::new();
-            let result = client.connect_to_server(ip, port, user, password, id_client);
-            tx.send(result).unwrap();
-        });
-    }));
 
-    publish_message.connect_clicked(clone!(@weak message_input, @weak topic_input => move |_| {
+        let (send_x, rec_x): (Sender<String>, Receiver<String>) = mpsc::channel();
+        sender_connect
+            .send((
+                ClientAction::Connect,
+                Some(ip),
+                Some(port),
+                Some(user),
+                Some(password),
+                Some(id_client),
+                send_x,
+            ))
+            .unwrap();
+
+        let result = rec_x.recv().unwrap();
+        tx.send(result).unwrap();
+    });
+
+    publish_button.connect_clicked(move |_| {
         let message = message_input.text().to_string();
         let topic = topic_input.text().to_string();
-        thread::spawn(move || {
-            
-        });
-    }));
+        let (send_x, rec_x): (Sender<String>, Receiver<String>) = mpsc::channel();
+        sender_publish
+            .send((
+                ClientAction::Publish,
+                Some(message),
+                Some(topic),
+                None,
+                None,
+                None,
+                send_x,
+            ))
+            .unwrap();
+    });
 
     rc.attach(None, move |text| {
-        match text {
-            Ok(label) => {
-                result_label.set_text(&label);
-            }
-            Err(label) => {
-                result_label.set_text(&label);
-            }
-        }
-
+        result_label.set_text(&text);
         glib::Continue(true)
     });
 
@@ -77,8 +112,8 @@ fn main() {
         gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default());
 
     application.connect_activate(|app| {
-        let client = Client::new();
-        build_ui_for_client(app, client);
+        let sender_client = Client::init();
+        build_ui_for_client(app, sender_client);
     });
 
     application.run();
--- client/src/client_for_interface.rs
+++ client/src/client_for_interface.rs
@@ -6,24 +6,98 @@ use std::{
 
 use crate::packet_manager::PacketManager;
 use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
 
 pub struct Client {
     stream: Option<TcpStream>,
+    sender: Option<Sender<(usize, Vec<String>)>>,
 }
 
-impl Default for Client {
-    fn default() -> Self {
-        Self::new()
+impl Clone for Client {
+    fn clone(&self) -> Self {
+        if let Some(stream) = &self.stream {
+            if let Ok(stream) = stream.try_clone() {
+                return Client {
+                    stream: Some(stream),
+                    sender: self.sender.clone(),
+                };
+            }
+            return Client {
+                stream: None,
+                sender: None,
+            };
+        }
+        return Client {
+            stream: None,
+            sender: None,
+        };
     }
 }
+
+pub enum ClientAction {
+    Connect,
+    Publish,
+    Subscribe,
+}
+
+pub type SenderClient = (
+    ClientAction,
+    Option<String>,
+    Option<String>,
+    Option<String>,
+    Option<String>,
+    Option<String>,
+    Sender<String>,
+);
 
 impl Client {
-    pub fn new() -> Client {
-        Client { stream: None }
+    pub fn init() -> Sender<SenderClient> {
+        let (sender, receiver): (Sender<SenderClient>, Receiver<SenderClient>) = mpsc::channel();
+
+        let mut client = Client {
+            stream: None,
+            sender: None,
+        };
+
+        thread::spawn(move || {
+            for receive in receiver {
+                match receive {
+                    (
+                        ClientAction::Connect,
+                        Some(host),
+                        Some(port),
+                        Some(user),
+                        Some(password),
+                        Some(id_client),
+                        sender_response,
+                    ) => {
+                        let response =
+                            client.connect_to_server(host, port, user, password, id_client);
+                        sender_response.send(response).unwrap();
+                    }
+                    (
+                        ClientAction::Publish,
+                        Some(message),
+                        Some(topic),
+                        None,
+                        None,
+                        None,
+                        sender_response,
+                    ) => {
+                        client.publish_message(message, topic);
+                    }
+                    _ => panic!("Algo mal"),
+                }
+            }
+        });
+
+        sender
     }
 
-    fn send_connect(&self, user: String, password: String, stream: &TcpStream, id_client: String) {
-        match Option::Some(stream) {
+    fn send_connect(&self, user: String, password: String, id_client: String) {
+        let client_cloned = self.clone();
+        match client_cloned.stream {
             Some(mut stream) => {
                 let mut flags: u8 = 0x00;
                 let mut bytes = vec![
@@ -40,46 +114,85 @@ impl Client {
                 bytes[8] = flags;
                 let length = bytes.len();
                 bytes.insert(1, (length - 1) as u8);
+                println!("{:?}", bytes);
                 stream.write_all(&bytes).unwrap();
             }
             None => panic!("No pude enviar"),
         }
     }
 
-    fn check_connack_code(&self, code: u8) -> Result<String, String> {
+    pub fn publish_message(&self, message: String, topic: String) {
+        let client_clone = self.clone();
+        match client_clone.stream {
+            Some(mut stream) => {
+                let mut bytes = vec![
+                    0x31, // Paquete publish QoS 0
+                         //0x09,  Remaining Length
+                ];
+                self.add_topic_bytes(topic, &mut bytes);
+                self.add_message_bytes(message, &mut bytes);
+                let length = bytes.len();
+                bytes.insert(1, (length - 1) as u8);
+                stream.write_all(&bytes).unwrap();
+            }
+            None => panic!("No pude enviar"),
+        }
+    }
+
+    fn check_connack_code(&self, code: u8) -> String {
         match code {
-            0x00 => Ok("Conexion realizada con exito".to_string()),
-            0x01 => Err("Error: la version del protocolo no es compatible".to_string()),
-            0x04 => Err("Error: los datos enviados no son correctos".to_string()),
-            0x05 => Err("Error: no esta autorizado".to_string()),
-            _ => Err("Error desconocido".to_string()),
+            0x00 => "Conexion realizada con exito".to_string(),
+            0x01 => "Error: la version del protocolo no es compatible".to_string(),
+            0x04 => "Error: los datos enviados no son correctos".to_string(),
+            0x05 => "Error: no esta autorizado".to_string(),
+            _ => "Error desconocido".to_string(),
+        }
+    }
+
+    fn add_topic_bytes(&self, topic: String, bytes: &mut Vec<u8>) {
+        if !topic.is_empty() {
+            let topic_length = topic.len();
+            let mut topic_in_bytes = topic.as_bytes().to_vec();
+            bytes.push(0x00);
+            bytes.push(topic_length as u8);
+            bytes.append(&mut topic_in_bytes);
+        }
+    }
+
+    fn add_message_bytes(&self, message: String, bytes: &mut Vec<u8>) {
+        if !message.is_empty() {
+            let message_length = message.len();
+            let mut message_in_bytes = message.as_bytes().to_vec();
+            bytes.push(0x00);
+            bytes.push(message_length as u8);
+            bytes.append(&mut message_in_bytes);
         }
     }
 
     pub fn connect_to_server(
-        mut self,
+        &mut self,
         host: String,
         port: String,
         user: String,
         password: String,
         id_client: String,
-    ) -> Result<String, String> {
+    ) -> String {
         let address = format!("{}:{}", host, port);
         match TcpStream::connect(address) {
             Ok(stream) => {
                 let (tx, rx) = mpsc::channel::<u8>();
                 let stream_copy = stream.try_clone().expect("Could not clone");
-                self.send_connect(user, password, &stream_copy, id_client);
+                self.stream = Some(stream);
+                self.send_connect(user, password, id_client);
                 thread::spawn(move || {
                     receive_responses_from_broker(stream_copy, tx);
                 });
                 let connack_code_received = rx.recv().unwrap();
-                self.stream = Some(stream);
                 self.check_connack_code(connack_code_received)
             }
             Err(e) => {
                 println!("Failed to connect: {}", e);
-                Err("La conexion no se ha podido establecer".to_string())
+                "La conexion no se ha podido establecer".to_string()
             }
         }
     }
