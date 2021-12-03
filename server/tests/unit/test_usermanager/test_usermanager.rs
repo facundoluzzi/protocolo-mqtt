3c85eb183c2e6edc978fc40d0eee3a6313d2961c
argelignacio <iargel@fi.uba.ar>
2021-12-03 14:15:04 -0300

    testeamos funcionalidades hechas hasta ahora en usermanager

--- server/tests/unit/test_usermanager/test_usermanager.rs
+++ server/tests/unit/test_usermanager/test_usermanager.rs
@@ -57,4 +57,29 @@ use server::stream::stream_handler::StreamType;
             Err(_) =>{ assert_eq!(1,1) }
         }
     }
+    #[test]
+    fn should_add_a_user_and_disconnect_and_reconnect_give_some_sender() {
+        let sender = UserManager::init();
+        let (sender_extraction, receiver_extraction): (Sender<Sender<String>>, Receiver<Sender<String>>) = mpsc::channel();
+        let (sender_stream, receiver_stream): (Sender<StreamType>, Receiver<StreamType>) = mpsc::channel();
+        sender.send((AddUserManager,"Nacho".to_owned(),Some(sender_stream), Some(false),None)).unwrap();
+        sender.send((GetSenderUserManager,"Nacho".to_owned(),None, None,Some(sender_extraction.clone()))).unwrap();
+        match  receiver_extraction.recv(){
+            Ok(_) =>{ assert_eq!(1,1) }
+            Err(_) =>{ assert_eq!(0,1) }
+        }
+        sender.send((DisconectUserManager,"Nacho".to_owned(),None,None,None)).unwrap();
+        sender.send((GetSenderUserManager,"Nacho".to_owned(),None, None,Some(sender_extraction))).unwrap();
+        match  receiver_extraction.recv(){
+            Ok(_) =>{ assert_eq!(0,1) }
+            Err(_) =>{ assert_eq!(1,1) }
+        }
+        sender.send((AddUserManager,"Nacho".to_owned(),Some(sender_stream), Some(false),None)).unwrap();
+        sender.send((GetSenderUserManager,"Nacho".to_owned(),None, None,Some(sender_extraction.clone()))).unwrap();
+        match  receiver_extraction.recv(){
+            Ok(_) =>{ assert_eq!(1,1) }
+            Err(_) =>{ assert_eq!(0,1) }
+        }
+
+    }
 }
