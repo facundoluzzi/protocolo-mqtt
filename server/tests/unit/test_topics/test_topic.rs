dc54916e86c55e7a5ad95b6228ebb9801e127de8
argelignacio <iargel@fi.uba.ar>
2021-12-04 23:27:40 -0300

    elimino warnings

--- server/tests/unit/test_topics/test_topic.rs
+++ server/tests/unit/test_topics/test_topic.rs
@@ -1,20 +1,17 @@
 #[cfg(test)]
 mod tests {
     use server::topics::topic_actions::TopicAction::{AddTopic, PublishMessage, RemoveTopic};
-    use server::usermanager::user_manager_action::UserManagerAction::PublishMessageUserManager;
     use server::usermanager::user_manager_types::ChannelUserManager;
     use std::sync::mpsc;
     use std::sync::mpsc::Receiver;
     use std::sync::mpsc::Sender;
-    use std::sync::Arc;
-    use std::sync::Mutex;
-    use std::thread;
+
 
     use server::topics::topic::Topic;
 
     #[test]
     fn should_add_topic_and_publish_message() {
-        let mut topic = Topic::init("ALTEGO".to_owned());
+        let topic = Topic::init("ALTEGO".to_owned());
 
         let (sender_one, receiver_one): (Sender<ChannelUserManager>, Receiver<ChannelUserManager>) =
             mpsc::channel();
@@ -50,7 +47,7 @@ mod tests {
     }
     #[test]
     fn create_topic_add_two_subscribers_remove_one_and_publish_message() {
-        let mut topic = Topic::init("ALTEGO".to_owned());
+        let topic = Topic::init("ALTEGO".to_owned());
 
         let (sender_one, receiver_one): (Sender<ChannelUserManager>, Receiver<ChannelUserManager>) =
             mpsc::channel();
