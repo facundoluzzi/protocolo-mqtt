0b08e8633f8c4b325bce6417da153601cdee978b
argelignacio <iargel@fi.uba.ar>
2021-12-15 12:11:01 -0300

    listo para meergear

--- server/src/topic/topics.rs
+++ server/src/topic/topics.rs
@@ -68,7 +68,6 @@ impl Topic {
 
     fn remove(&mut self, subscriber: String) {
         self.subscribers.remove(&subscriber);
-        println!("Eliminado :D: {}", self.subscribers.len());
     }
 
     fn publish_retained_message(
@@ -106,11 +105,6 @@ impl Topic {
                 new_packet.remove(index_to_delete);
                 new_packet[1] -= 2;
             }
-            println!(
-                "VIEJO: {:?}.... NUEVO: {:?}",
-                packet.len(),
-                new_packet.len()
-            );
             let action = UserManagerAction::PublishMessageUserManager(
                 PublishMessageUserManager::init(client_id.to_string(), new_packet.clone()),
             );
