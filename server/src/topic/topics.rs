901704ed0b3d3b08bfe933d6139a9384ad260c7b
facundoluzzi <facundoluzz@gmail.com>
2021-12-15 16:10:46 -0300

    Solucione bugs en el funcionalidad de retained message

--- server/src/topic/topics.rs
+++ server/src/topic/topics.rs
@@ -41,16 +41,18 @@ impl Topic {
                         let qos = action.get_qos();
                         let message = action.get_message();
                         let retained_message = action.get_retained_message();
-                        if packet.is_empty() && retained_message {
-                            topic.retained_message = None;
-                        } else if retained_message && qos == 0 {
-                            let publish = PublishMessage::init(
-                                packet.clone(),
-                                qos,
-                                true,
-                                message.to_string(),
-                            );
-                            topic.retained_message = Some(publish);
+                        if retained_message {
+                            if packet.is_empty() {
+                                topic.retained_message = None;
+                            } else {
+                                let publish = PublishMessage::init(
+                                    packet.clone(),
+                                    qos,
+                                    true,
+                                    message.to_string(),
+                                );
+                                topic.retained_message = Some(publish);
+                            }
                         }
 
                         topic.publish_msg(packet, qos, message);
