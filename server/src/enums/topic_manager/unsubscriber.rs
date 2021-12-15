483b2febfc147a64f876c1ff102a459e0fb74dcb
facundoluzzi <facundoluzz@gmail.com>
2021-12-15 18:18:37 -0300

    Unsubscribe funciona ahora con wildcards

--- server/src/enums/topic_manager/unsubscriber.rs
+++ server/src/enums/topic_manager/unsubscriber.rs
@@ -1,26 +1,66 @@
 use crate::enums::topic::remove_topic::RemoveTopic;
 use crate::enums::topic::topic_actions::TopicAction;
+use crate::wildcard::wildcard_handler::Wildcard;
 use std::collections::HashMap;
 use std::sync::mpsc::Sender;
 
 pub struct Unsubscriber {
     client_id: String,
     topic: String,
+    wildcard: Option<Wildcard>,
 }
 
 impl Unsubscriber {
-    pub fn init(client_id: String, topic: String) -> Unsubscriber {
-        Unsubscriber { client_id, topic }
+    pub fn init(client_id: String, topic: String, wildcard: Option<Wildcard>) -> Unsubscriber {
+        Unsubscriber {
+            client_id,
+            topic,
+            wildcard,
+        }
     }
 
     pub fn unsubscribe(
         &mut self,
         topics: HashMap<String, Sender<TopicAction>>,
     ) -> HashMap<String, Sender<TopicAction>> {
+        match &self.wildcard {
+            Some(wildcard) => self.unsubscribe_with_wilcard(wildcard.clone(), topics),
+            None => self.unsubscribe_without_wilcard(topics),
+        }
+        // if let Some(topic_sender) = topics.get(&self.topic.to_owned()) {
+        //     let remove_topic = TopicAction::Remove(RemoveTopic::init(self.client_id.to_owned()));
+        //     topic_sender.send(remove_topic).unwrap();
+        // }
+        // topics
+    }
+
+    fn unsubscribe_with_wilcard(
+        &self,
+        wildcard: Wildcard,
+        topics: HashMap<String, Sender<TopicAction>>,
+    ) -> HashMap<String, Sender<TopicAction>> {
         if let Some(topic_sender) = topics.get(&self.topic.to_owned()) {
             let remove_topic = TopicAction::Remove(RemoveTopic::init(self.client_id.to_owned()));
             topic_sender.send(remove_topic).unwrap();
-            println!("Suscriptor eliminado!: topic: {}", self.topic.to_owned());
+        }
+
+        for (topic_name, topic_sender) in &topics {
+            if wildcard.verify_topic(topic_name.to_owned()) {
+                let remove_topic =
+                    TopicAction::Remove(RemoveTopic::init(self.client_id.to_owned()));
+                topic_sender.send(remove_topic).unwrap();
+            }
+        }
+        topics
+    }
+
+    fn unsubscribe_without_wilcard(
+        &self,
+        topics: HashMap<String, Sender<TopicAction>>,
+    ) -> HashMap<String, Sender<TopicAction>> {
+        if let Some(topic_sender) = topics.get(&self.topic.to_owned()) {
+            let remove_topic = TopicAction::Remove(RemoveTopic::init(self.client_id.to_owned()));
+            topic_sender.send(remove_topic).unwrap();
         }
         topics
     }
