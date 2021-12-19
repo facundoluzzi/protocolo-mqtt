193b5b2ee74dc850922b406ad275db1e33bd2ec3
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2021-12-19 18:44:39 -0300

    refactor de stream handler

--- server/src/stream/stream_handler.rs
+++ server/src/stream/stream_handler.rs
@@ -1,3 +1,4 @@
+use std::io::Error;
 use crate::helper::remaining_length::save_remaining_length;
 use crate::logs::logger::Logger;
 use std::net::Shutdown;
@@ -30,100 +31,78 @@ pub type StreamType = (
 );
 
 impl Stream {
-    fn get_streams(stream: TcpStream) -> Result<(TcpStream, TcpStream), String> {
-        match (stream.try_clone(), stream.try_clone()) {
-            (Ok(stream_to_read), Ok(stream_to_write)) => Ok((stream_to_read, stream_to_write)),
-            _ => Err("Unexpected error".to_string()),
-        }
-    }
-
-    fn get_cloned_stream(stream: Result<TcpStream, std::io::Error>) -> Result<TcpStream, String> {
-        match stream {
-            Ok(stream) => Ok(stream),
-            Err(_err) => Err("Unexpected error cloning stream".to_string()),
-        }
-    }
-
     fn process_input_and_output(
+        message_received: StreamType,
         to_write: TcpStream,
-        to_read: TcpStream,
-        receiver: Receiver<StreamType>,
-        logger: Logger,
-    ) -> Result<(), String> {
-        for message_received in receiver {
-            let action = message_received.0;
-            let cloned_to_write = Stream::get_cloned_stream(to_write.try_clone())?;
-            let cloned_to_read = Stream::get_cloned_stream(to_read.try_clone())?;
-            match action {
-                StreamAction::WriteStream => {
-                    Stream::write(cloned_to_write, message_received.1, logger.clone());
-                }
-                StreamAction::ReadStream => {
-                    if let Some(sender) = message_received.2 {
-                        let cloned_logger = logger.clone();
-                        thread::spawn(move || {
-                            Stream::read(cloned_to_read, cloned_to_write, sender, cloned_logger);
-                        });
-                    } else {
-                        panic!("Unexpected error: send a Some(Sender<String>) here");
-                    }
-                }
-                StreamAction::CloseConnectionStream => {
-                    Stream::close_streams(cloned_to_write, cloned_to_read, logger.clone())
-                }
-                StreamAction::SetKeepAliveStream => {
-                    if let Err(err) = cloned_to_read.set_read_timeout(message_received.3) {
-                        println!("Unexpected error setting keep alive: {}", err);
-                    }
+        to_read: TcpStream
+    ) -> Result<(), Error> {
+        let action = message_received.0;
+        match action {
+            StreamAction::WriteStream => {
+                Stream::write(to_write, message_received.1)
+            }
+            StreamAction::ReadStream => {
+                if let Some(sender) = message_received.2 {
+                    thread::spawn(move || -> Result<(), Error> {
+                        Stream::read(to_read, to_write, sender)
+                    });
+                    Ok(())
+                } else {
+                    panic!("Unexpected error: send a Some(Sender<String>) here");
                 }
             }
+            StreamAction::CloseConnectionStream => Stream::close_streams(to_write, to_read),
+            StreamAction::SetKeepAliveStream => to_read.set_read_timeout(message_received.3),
         }
-        Ok(())
     }
 
     /// Recibe un tcp stream para poder escribir y leer al mismo tiempo. Lanza un thread que se queda escuchando eventos
     /// de diferentes partes de la app
-    pub fn init(stream_received: TcpStream, logger: Logger) -> Result<Sender<StreamType>, String> {
+    pub fn init(
+        stream_received: TcpStream,
+        mut logger: Logger,
+    ) -> Result<Sender<StreamType>, Error> {
         let (sender_stream, receiver_stream): ChannelStreamType = mpsc::channel();
-        let (stream_to_read, stream_to_write) = Stream::get_streams(stream_received)?;
-        std::thread::spawn(move || {
-            let result_process = Stream::process_input_and_output(
-                stream_to_write,
-                stream_to_read,
-                receiver_stream,
-                logger,
-            );
-            if let Err(err) = result_process {
-                println!("{}", err);
+        thread::spawn(move || -> Result<(), Error> {
+            for message_received in receiver_stream {
+                if let Err(err) = Stream::process_input_and_output(
+                    message_received,
+                    stream_received.try_clone()?,
+                    stream_received.try_clone()?
+                ) {
+                    logger.info(format!("Unexpected error in logger: {}", err.to_string()));
+                    return Err(err);
+                } else {
+                    return Ok(());
+                }
             }
+            Ok(())
         });
         Ok(sender_stream)
     }
 
-    fn write(mut stream: TcpStream, message: Option<Vec<u8>>, mut logger: Logger) {
+    fn write(mut stream: TcpStream, message: Option<Vec<u8>>) -> Result<(), Error> {
         match message {
             Some(message) => {
                 let message_to_write: &[u8] = &message;
-                if let Err(msg_error) = stream.write(message_to_write) {
-                    logger.info(format!("Error in sending response: {}", msg_error));
-                }
+                stream.write(message_to_write)?;
+                Ok(())
             }
             None => panic!("Unexpected error: send a Some(message) here"),
-        };
+        }
     }
 
     fn read(
         mut stream: TcpStream,
         stream_to_write: TcpStream,
-        sender: Sender<Vec<u8>>,
-        logger: Logger,
-    ) {
+        sender: Sender<Vec<u8>>
+    ) -> Result<(), Error> {
         let mut total_data: Vec<u8> = Vec::new();
         let mut is_first_byte = true;
         let mut packet_length = 0;
         let mut readed_bytes = 0;
         let mut data = [0_u8; 5];
-        while match stream.read(&mut data) {
+        Ok(while match stream.read(&mut data) {
             Ok(size) => {
                 total_data = Stream::build_total_data(
                     &mut data,
@@ -142,13 +121,12 @@ impl Stream {
             }
             Err(_err) => {
                 Stream::close_streams(
-                    stream.try_clone().unwrap(),
-                    stream_to_write.try_clone().unwrap(),
-                    logger.clone(),
-                );
+                    stream.try_clone()?,
+                    stream_to_write.try_clone()?
+                )?;
                 true
             }
-        } {}
+        } {})
     }
 
     fn build_total_data(
@@ -188,13 +166,9 @@ impl Stream {
         }
     }
 
-    fn close_streams(stream_to_write: TcpStream, stream_to_read: TcpStream, mut logger: Logger) {
-        if let Err(err_msg) = stream_to_read.shutdown(Shutdown::Both) {
-            logger.info(format!("Unexpected error closing the stream: {}", err_msg));
-        }
-
-        if let Err(err_msg) = stream_to_write.shutdown(Shutdown::Both) {
-            logger.info(format!("Unexpected error closing the stream: {}", err_msg));
-        }
+    fn close_streams(stream_to_write: TcpStream, stream_to_read: TcpStream) -> Result<(), Error> {
+        stream_to_read.shutdown(Shutdown::Both)?;
+        stream_to_write.shutdown(Shutdown::Both)?;
+        Ok(())
     }
 }
