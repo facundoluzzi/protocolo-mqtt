260ded163cf7673bf24a13f9a7c22d38d35f4079
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2022-01-16 17:42:49 -0300

    terminamos parte a

---
+++ fake-generator/src/main.rs
@@ -0,0 +1,85 @@
+use std::io::Write;
+use fake_generator::helper::stream::stream_handler::Stream;
+use fake_generator::helper::stream::stream_handler::StreamAction::WriteStream;
+use fake_generator::types::StreamType;
+use std::net::TcpStream;
+use std::sync::mpsc::Sender;
+use rand::Rng;
+use std::thread;
+use std::time::Duration;
+
+/// Construye el vector de bytes que van a representar al paquete Publish y los devuelve
+fn build_bytes_for_connect() -> Vec<u8> {
+    [
+        0x10, 
+        0x12,
+        0x00, 0x04, 0x4D, 0x51, 0x54, 0x54, 
+        0x04, 
+        0x00, 
+        0x00, 0x00, 
+        0x00, 0x06, 0x41, 0x4C, 0x54, 0x45, 0x47, 0x4F, 
+    ].to_vec()
+}
+
+/// Obtiene la serie de bytes que van a representar al paquete Connect, y manda una accion de escritura hacia el Stream, para que
+/// lo mande hacia el broker
+fn send_connect(mut stream: TcpStream) -> Result<(), String> {
+    let connect_bytes = build_bytes_for_connect();
+    stream.write(&connect_bytes).unwrap();
+    Ok(())
+}
+
+pub fn connect_to_server() -> Result<TcpStream, String> {
+    let address = "0.0.0.0:1883".to_owned();
+    match TcpStream::connect(address) {
+        Ok(stream) => {
+            send_connect(stream.try_clone().unwrap()).unwrap();
+            Ok(stream.try_clone().unwrap())
+        }
+        Err(err) => {
+            println!("Failed to connect: {}", err);
+            let response = "La conexion no se ha podido establecer".to_string();
+            Err(response)
+        }
+    }
+}
+
+/// Construye el vector de bytes que van a representar al paquete Publish y los devuelve
+fn build_bytes_for_subscribe() -> Vec<u8> {
+    [0x82, 0x09, 0x00, 0x0A, 0x00, 0x04, 0x54, 0x45, 0x4D, 0x50, 0x00].to_vec()
+}
+
+pub fn send_subscribe(mut stream: TcpStream) -> Result<(), String> {
+    let subscribe_bytes = build_bytes_for_subscribe();
+    stream.write(&subscribe_bytes).unwrap();
+    Ok(())
+}
+
+fn random_value() -> [u8; 2] {
+    let unit = rand::thread_rng().gen_range(48..57);
+    let ten = rand::thread_rng().gen_range(48..53);
+    [ten, unit]
+}
+
+/// Construye el vector de bytes que van a representar al paquete Publish y los devuelve
+fn build_bytes_for_publish(ten: u8, unit: u8) -> Vec<u8> {
+    [0x30, 0x08, 0x00, 0x04, 0x54, 0x45, 0x4D, 0x50, ten, unit].to_vec()
+}
+
+pub fn send_publish(mut stream: TcpStream, ten: u8, unit: u8) -> Result<(), String> {
+    let publish_bytes = build_bytes_for_publish(ten, unit);
+    stream.write(&publish_bytes).unwrap();
+    Ok(())
+}
+
+fn main() {
+    let stream = connect_to_server().unwrap();
+    send_subscribe(stream.try_clone().unwrap()).unwrap();
+    thread::sleep(Duration::from_secs(1));
+    loop {
+        thread::sleep(Duration::from_secs(5));
+        let [ten, unit]: [u8; 2] = random_value();
+        send_publish(stream.try_clone().unwrap(), ten, unit).unwrap();
+        println!("{}{}", ten, unit);
+    }
+}
--- makefile
+++ makefile
@@ -1,8 +1,10 @@
 SERVER_TOML_PATH=--manifest-path=server/Cargo.toml
 CLIENT_TOML_PATH=--manifest-path=client/Cargo.toml
+FAKE_GENERATOR_PATH=--manifest-path=fake-generator/Cargo.toml
 
 .PHONY: server
 .PHONY: client
+.PHONY: fake_generator
 .PHONY: graphics
 
 server: server/src/main.rs
@@ -14,6 +16,11 @@ client: client/src/main.rs
 	cargo fmt $(CLIENT_TOML_PATH)
 	cargo clippy $(CLIENT_TOML_PATH)
 	cargo run --bin client $(CLIENT_TOML_PATH)
+
+fake_generator: fake-generator/src/main.rs
+	cargo fmt $(FAKE_GENERATOR_PATH)
+	cargo clippy $(FAKE_GENERATOR_PATH)
+	cargo run $(FAKE_GENERATOR_PATH) server.conf
 
 server-test:
 	cargo test $(SERVER_TOML_PATH)
